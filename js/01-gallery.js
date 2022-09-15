0bc256b57ae4089e5602a0de86412d7aeb84fe0b
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-15 23:09:57 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+console.log(galleryRef);
