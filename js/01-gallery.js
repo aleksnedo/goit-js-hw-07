9354f813cb5e01c02073e3f748b3f906ebda9689
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-17 00:33:33 +0300

    task 01 update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,4 +4,47 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const galleryRef = document.querySelector(".gallery");
-console.log(galleryRef);
+const gallaryMarkup = galleryCreateMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", gallaryMarkup);
+
+function galleryCreateMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>`;
+    })
+    .join("");
+}
+
+galleryRef.addEventListener("click", onOpenPictureClick);
+
+function onOpenPictureClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const galleryOriginalPicture = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${galleryOriginalPicture}" width="800" height="600">
+`);
+  instance.show();
+
+  galleryRef.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
